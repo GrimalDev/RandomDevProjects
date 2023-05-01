a2052056c6463c7d3486117011cc879f4b806bbf
Grimaldi Baptiste <79135575+GrimalDev@users.noreply.github.com>
2023-05-01 10:32:34 +0200

    Delete Mediocs-fullstack-page directory

--- Mediocs-fullstack-page/app/config/bcrypt-config.js
+++
@@ -1,4 +0,0 @@
-module.exports = async function hashPassword(bcrypt, toHash) {
-    const hashedString = await bcrypt.hash(toHash, 10)
-    return hashedString
-}
--- Mediocs-fullstack-page/app/config/userDataModify-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function modifyUserData(req, res, user_database, bcrypt) {
-    const { power, nom, prenom, email, password, confirmPassword } = req.body;
-
-    
-
-    user_database.update()
-
-}
